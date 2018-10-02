da940c9966dadd6d6f8eaef5b1a7d078034efbb2
AnastasiaVedernikova <vedernikova@ucu.edu.ua>
2018-10-02 16:20:33 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main {
         Point point = new Point(2.78, 4.616);
         Point other = new Point(1.57, 2.214);
         point.add(other);
-        //result should be x= -0.409, y = -1.715
+        //result should be x= -0.409, y = 1.715
         System.out.print(point.x);
         System.out.print(point.y);
     }
